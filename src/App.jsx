2ff463baffe0b1a1ce9a4a0d4db5dfd55c2ffd4c
BLESSED <blessed@BLESSEDs-MacBook-Pro.local>
2024-01-30 05:53:24 -0500

    Added Funcationalities and State Hook, Learnt How to use Use State Counters and Alerts

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,23 @@ import {Header} from './components/Header'
 import {Form} from './components/Form'
 import './App.css'
 
-function App() {
+function App()  {
+    // const [counter, setCounter] = useState(0)
 
   return (
     <>
       <div className='main-wrapper'>
           
           <div className='sub-wrapper'>
+            {/* value of counterValue is {counter}
+            <button onClick={() => setCounter(counter + 1)}>
+              Increment
+            </button>
+            {" "} {" "} 
+            <button onClick={() => setCounter(counter - 1)}>
+              Decrement
+            </button> */}
+
             <Header/>
             <Form/>
           </div>
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,5 +1,29 @@
 import '../styles/Form.css'
+import { useState } from 'react'
+
 export const Form = () => {
+
+    const [firstName, setFirstName] = useState('')
+    const [lastName, setLastName] = useState('')
+    const [email, setEmail] = useState('')
+    const [company, setCompany] = useState('')
+
+    const handleSubmit = () => {
+        alert (
+
+                `First Name is ${firstName}\n` + 
+                `Last Name is ${lastName}\n` + 
+                `Email is ${email}\n` + 
+                `Company is ${company}\n`  
+        )
+
+        setFirstName('')
+        setLastName('')
+        setEmail('')
+        setCompany ('')
+    }
+
+
     return (
         <div className='user-details'>
                
@@ -11,23 +35,27 @@ export const Form = () => {
                         
                             <div className='input-box'>
                                 <label htmlFor="First Name">First Name</label>
-                                <input type="text" placeholder="First Name" required/>
+                                <input onChange={(e)=>{setFirstName(e.target.value)}}  value={firstName} type="text" placeholder="First Name" required/>
+                                {firstName}
                             </div>
 
                             <div className='input-box'>
                                 <label htmlFor="Last Name">Last Name</label>
-                                <input type="text" placeholder="Last Name" required/>
+                                <input onChange={(e)=>{setLastName(e.target.value)}}  value={lastName} type="text" placeholder="Last Name" required/>
+                                {lastName}
                             </div>
                         </div>
                             
                             <div className='input-box'>
                                 <label htmlFor="email">Email Address</label>
-                                <input type="email" placeholder="Email Address" required/>
+                                <input onChange={(e)=>{setEmail(e.target.value)}} value={email} type="email" placeholder="Email Address" required/>
+                                {email}
                             </div>
-
+                            
                             <div className='input-box'>
                                 <label htmlFor="Company">Company</label>
-                                <input type="text" placeholder="Company" required/>
+                                <input onChange={(e)=>{setCompany(e.target.value)}} value={company} type="text" placeholder="Company" required/>
+                                {company}
                             </div>
                             
                             <span>Add me to your mailing list</span>
@@ -37,7 +65,7 @@ export const Form = () => {
                             </div>
 
                              <div className="button-submit">
-                                <button>Sign Me Up</button>
+                                <button onClick={handleSubmit}>Sign Me Up</button>
                              </div>
 
 
